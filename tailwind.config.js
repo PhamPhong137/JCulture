d8748237372a90b540568767035f5b2fc5c337a4
Phong <pp072003@gmail.com>
2023-02-15 10:19:44 +0700

    Van hoa Nhat Ban

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html","./src/input.css"],
+  content: ["index.html","./src/input.css","./node_modules/flowbite/index.html","./node_modules/flowbite/input.css"],
+  darkMode:'class',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')
+],
 }
